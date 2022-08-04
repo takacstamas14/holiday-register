84801f4580f04d6d0fb1cc0359a4c646871f3118
tomikaaa14 <takacst7200@gmail.com>
2022-08-04 22:32:02 +0200

    initial commit

--- models/Holiday.js
+++ models/Holiday.js
@@ -1,7 +1,7 @@
 import {DataTypes, Sequelize} from "sequelize";
 import db from "../config/index.js";
 import User from "./User.js";
-const Register = db.define('registers',{
+const Holiday = db.define('holidays',{
     id: {
         type: DataTypes.INTEGER,
         autoIncrement: true,
@@ -15,7 +15,7 @@ const Register = db.define('registers',{
     }
 });
 
-User.belongsToMany(Register,{through: {model: Register,unique: false}})
-Register.belongsToMany(User,{through: {model: User,unique: false}})
+User.belongsToMany(Holiday,{through: {model: Holiday,unique: false}})
+Holiday.belongsToMany(User,{through: {model: User,unique: false}})
 
-export default Register;
+export default Holiday;
