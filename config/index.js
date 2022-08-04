ab38e064a841a77912e8e9fc37edd6e5d7af52c4
tomikaaa14 <takacst7200@gmail.com>
2022-08-04 22:18:41 +0200

    initial commit

--- config/index.js
+++ config/index.js
@@ -1,7 +1,5 @@
 import {Sequelize} from "sequelize";
 //const db = new Sequelize('postgres://ihbmtxjpcedtwx:3f00172acb811a67092e5d836b85215806eb0d250d4fe6c75b2a6d5d606d7485@ec2-52-208-164-5.eu-west-1.compute.amazonaws.com:5432/dd11a4iv7pf7ge') // Example for postgres
-
-const Sequelize = require('sequelize');
 const db = new Sequelize('postgres://ihbmtxjpcedtwx:3f00172acb811a67092e5d836b85215806eb0d250d4fe6c75b2a6d5d606d7485@ec2-52-208-164-5.eu-west-1.compute.amazonaws.com:5432/dd11a4iv7pf7ge', {
         dialectOptions: {
             ssl: {
