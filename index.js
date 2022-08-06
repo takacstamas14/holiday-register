808460d64adf3317f999026303df7d3355760e90
tomikaaa14 <takacst7200@gmail.com>
2022-08-06 14:06:35 +0200

    update

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(session({
-        key: "login",
+        key: "userId",
         secret: "secretkey",
         resave: true,
         saveUninitialized: false,
