2d872c94b44102f09a17ad93f789d47e7e848d44
Takács Tamás <takacst7200@gmail.com>
2022-08-08 19:08:28 +0200

    update

--- index.js
+++ index.js
@@ -32,7 +32,9 @@ const __dirname = path.dirname(__filename);
 
 const pub = path.join(__dirname, 'public','build','index.html');
 console.log(pub);
-app.use('/',express.static(pub));
+app.get('/',(req,res)=>{
+    res.sendFile(express.static(pub));
+});
 db.sync({alter: true, force: true}).then(result => {
     app.listen(process.env.PORT || 5000);
     //app.listen(3001, () => {console.log("running server");});
