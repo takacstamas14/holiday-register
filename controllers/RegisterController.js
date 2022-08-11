57b2e48c0d2abeb1ebbc45243ca62aa7caa35026
tomikaaa14 <takacst7200@gmail.com>
2022-08-11 23:00:21 +0200

    add functions

--- controllers/RegisterController.js
+++ controllers/RegisterController.js
@@ -1,4 +1,5 @@
 import Holiday from "../models/Holiday.js";
+import User from "../models/User";
 
 export const saveDate = async (req,res) => {
 
@@ -30,11 +31,16 @@ export const getRegistered = async (req,res) => {
         const registered = await Holiday.findAll({
             attributes: ['startDate', 'endDate']
         });
+        const user = await User.findAll({
+            where: {
+                id: req.session.userId
+            }
+        });
         const registeredString = JSON.stringify(registered);
         const arr = JSON.parse(registeredString);
         arr.forEach( obj => renameKey( obj, 'startDate', 'start' ) );
         arr.forEach( obj => renameKey( obj, 'endDate', 'end' ) );
-        arr.forEach( obj => obj.title = "Teszt");
+        arr.forEach( obj => obj.title = user.fullName);
         res.status(200).json({data: arr});
     } catch (e) {
         res.status(500).json({msg: e});
