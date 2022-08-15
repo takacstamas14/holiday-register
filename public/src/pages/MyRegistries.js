0135562dc9b32843cba0ef52055d1994fae2dc53
Takács Tamás <takacst7200@gmail.com>
2022-08-15 11:45:21 +0200

    update

--- public/src/pages/MyRegistries.js
+++ public/src/pages/MyRegistries.js
@@ -1,27 +1,51 @@
 import * as React from 'react';
 import {useEffect, useState} from "react";
 import axios from "axios";
+import {TableBody, TableCell, TableContainer, TableHead, TableRow} from "@mui/material";
+import {Table} from "@fullcalendar/daygrid";
+import Paper from "@mui/material/Paper";
 
 
 export default function MyRegistries() {
 const [data,setData] = useState(null);
-
+let rows = null;
 const getDataFromAPI = () => {
-
     axios.get("/api/getRegistered",{withCredentials:true}).then((response)=>{
         console.log(response.data);
         return response.data;
     })
-
 }
-
-
 useEffect(() => {
     setData(getDataFromAPI);
+    rows = [data];
 });
 
     return (<>
-        <h1>1</h1>
+        <TableContainer component={Paper}>
+            <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                <TableHead>
+                    <TableRow>
+                        <TableCell>Szabadság oka</TableCell>
+                        <TableCell align="right">Kezdődátum</TableCell>
+                        <TableCell align="right">Végdátum</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {rows.map((row) => (
+                        <TableRow
+                            key={row.title}
+                            sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                        >
+                            <TableCell component="th" scope="row">
+                                {row.title}
+                            </TableCell>
+                            <TableCell align="right">{row.start}</TableCell>
+                            <TableCell align="right">{row.end}</TableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
+        </TableContainer>
     </>);
 
 
