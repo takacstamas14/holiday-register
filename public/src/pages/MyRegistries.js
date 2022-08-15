08e1e450b628a79cb8d2e37d0fb3a26372e8477a
Takács Tamás <takacst7200@gmail.com>
2022-08-15 14:08:40 +0200

    update

--- public/src/pages/MyRegistries.js
+++ public/src/pages/MyRegistries.js
@@ -1,8 +1,7 @@
 import * as React from 'react';
 import {useEffect, useState} from "react";
 import axios from "axios";
-import {TableBody, TableCell, TableContainer, TableHead, TableRow} from "@mui/material";
-import {Table} from "@fullcalendar/daygrid";
+import {Table,TableBody, TableCell, TableContainer, TableHead, TableRow} from "@mui/material";
 import Paper from "@mui/material/Paper";
 
 
@@ -19,31 +18,31 @@ useEffect(() => {
 
     return (
         <>
-        <TableContainer component={Paper}>
-            <Table sx={{ minWidth: 650 }} aria-label="simple table">
-                <TableHead>
-                    <TableRow>
-                        <TableCell>Szabadság oka</TableCell>
-                        <TableCell align="right">Kezdődátum</TableCell>
-                        <TableCell align="right">Végdátum</TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {rows.map((row) => (
-                        <TableRow
-                            key={row.title}
-                            sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                        >
-                            <TableCell component="th" scope="row">
-                                {row.title}
-                            </TableCell>
-                            <TableCell align="right">{row.start}</TableCell>
-                            <TableCell align="right">{row.end}</TableCell>
+            <TableContainer component={Paper}>
+                <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>Szabadság oka</TableCell>
+                            <TableCell align="right">Kezdő dátum</TableCell>
+                            <TableCell align="right">Végdátum</TableCell>
                         </TableRow>
-                    ))}
-                </TableBody>
-            </Table>
-        </TableContainer>
+                    </TableHead>
+                    <TableBody>
+                        {rows.map((row) => (
+                            <TableRow
+                                key={row.title}
+                                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                            >
+                                <TableCell component="th" scope="row">
+                                    {row.title}
+                                </TableCell>
+                                <TableCell align="right">{row.start}</TableCell>
+                                <TableCell align="right">{row.end}</TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
     </>
     );
 
