c9e3892520c96c91adda70e070795cb88c225ebd
TakacsTamas <takacst7200@gmail.com>
2022-08-17 14:23:10 +0200

    update

--- public/src/pages/Home.js
+++ public/src/pages/Home.js
@@ -79,7 +79,7 @@ export default function Home() {
         eventClick={(info) => {handleEventClick(info)}}
 
     />
-            <Dialog open={open} onClose={handleClose}>
+            <Dialog open={open} onClose={handleClose} fullWidth={true} maxWidth={"sm"}>
                 <DialogTitle>{openedData.title}</DialogTitle>
                 <DialogContent dividers>
                     <List>
